74f043cbb4aabf9ee60d4472f183ed042e969d44
aliali <aliali@test.com>
2025-03-11 10:31:03 +0300

    first commit

---
+++ src/main/java/com/example/lab06_tuwaiqjavabootcamp2/Lab06TuwaiqJavaBootCamp2Application.java
@@ -0,0 +1,13 @@
+package com.example.lab06_tuwaiqjavabootcamp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab06TuwaiqJavaBootCamp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab06TuwaiqJavaBootCamp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab06_tuwaiqjavabootcamp2/Lab06TuwaiqJavaBootCamp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab06_tuwaiqjavabootcamp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab06TuwaiqJavaBootCamp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
