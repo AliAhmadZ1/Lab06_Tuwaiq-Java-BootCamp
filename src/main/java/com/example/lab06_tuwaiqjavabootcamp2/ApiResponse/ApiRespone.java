fc8a8430c57d03628c1b47b72c57b2f8c0b9663b
aliali <aliali@test.com>
2025-03-11 11:14:34 +0300

    Done!

---
+++ src/main/java/com/example/lab06_tuwaiqjavabootcamp2/ApiResponse/ApiRespone.java
@@ -0,0 +1,10 @@
+package com.example.lab06_tuwaiqjavabootcamp2.ApiResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiRespone {
+    private String message;
+}
